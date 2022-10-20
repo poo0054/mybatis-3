66b7571d6383877bd19db0c9fbb301ad049ff7c0
zhangzhi <poo0054@outlook.com>
2022-10-20 09:19:04 +0800

    初始化3.4版本

--- src/test/java/org/apache/ibatis/parsing/XPathParserTest.java
+++ src/test/java/org/apache/ibatis/parsing/XPathParserTest.java
@@ -1,35 +1,35 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2022 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.parsing;
 
-import static org.junit.Assert.assertEquals;
+import org.apache.ibatis.io.Resources;
+import org.junit.Test;
 
 import java.io.InputStream;
 
-import org.apache.ibatis.io.Resources;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class XPathParserTest {
 
   @Test
   public void shouldTestXPathParserMethods() throws Exception {
-    String resource = "resources/nodelet_test.xml";
+    String resource = "src/test/resources/nodelet_test.xml";
     InputStream inputStream = Resources.getResourceAsStream(resource);
     XPathParser parser = new XPathParser(inputStream, false, null, null);
-    assertEquals((Long)1970l, parser.evalLong("/employee/birth_date/year"));
+    assertEquals((Long) 1970l, parser.evalLong("/employee/birth_date/year"));
     assertEquals((short) 6, (short) parser.evalShort("/employee/birth_date/month"));
     assertEquals((Integer) 15, parser.evalInteger("/employee/birth_date/day"));
     assertEquals((Float) 5.8f, parser.evalFloat("/employee/height"));
