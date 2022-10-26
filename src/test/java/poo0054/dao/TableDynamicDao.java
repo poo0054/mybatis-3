4548a98c2ed3390afda8c7ffc2fefca996d76fad
poo0054 <poo0054@outlook.com>
2022-10-26 15:26:51 +0800

    update 许可

--- src/test/java/poo0054/dao/TableDynamicDao.java
+++ src/test/java/poo0054/dao/TableDynamicDao.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package poo0054.dao;
 
 import org.apache.ibatis.annotations.Param;
